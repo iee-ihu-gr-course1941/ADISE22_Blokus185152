3b57645eb6ceb5a9ad553c18c8274276fa40283a
Polihronis Varvaris <75493778+PolihronisVarvaris@users.noreply.github.com>
2022-12-11 03:44:35 +0200

    finish blokus1

--- www/chess.php
+++ www/chess.php
@@ -2,6 +2,8 @@
 
 require_once "../lib/dbconnect.php";
 require_once "../lib/board.php";
+require_once "../lib/game.php";
+
 
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
@@ -12,19 +14,30 @@ switch ($r=array_shift($request)){
 	case 'board' :
 		switch ($b=array_shift($request)){
                 	case '': 
-                        case null: handle_board($method);break;
-                        case 'piece': //handle_piece($method, $request[0],$request[1],$input);
-                        	break;   
-                        case 'player'://handle_player($method,$request[0],$input);
+                        case null: handle_board($method);
+                                break;
+                        case 'piece': 
+                                handle_piece($method, $request[0],$request[1],$input);
+                        	break;
+                        case 'player':
+                                handle_player($method,$request[0],$input);
                                 break;
                         default: header("HTTP/1.1 404 Not Found");
-                                break;
-		}
+                                break;           
+                        }
                 break;
-        default:
-                header("HTTP/1.1 404 Not Found");
+        case 'status':
+                if(sizeof($request)==0){handle_status($method);}
+                else{header("HTTP/1.1 404 Not Found");}
+                break;
+        case 'players':
+                handle_player($method,$request,$input);
+                break;
+        default: header("HTTP/1.1 404 Not Found");
                 exit;
 }
+
+
 
 function handle_board($method){
         if($method=='GET'){
@@ -32,7 +45,24 @@ function handle_board($method){
         }else if($method=='POST'){
                 reset_board();
         }else{
-                header('HTPP/1.1405 Method Not Allowed');
+                header('HTPP/1.1 405 Method Not Allowed');
         }
 }
+
+function handle_piece($method,$x,$y,$input){
+;
+}
+
+function handle_player($method,$p,$input){
+;       
+}
+
+function handle_status($method){
+        if($method=='GET'){
+                show_status();
+        }else{
+                header('HTPP/1.1 405 Method Not Allowed');
+        }
+}
+
 ?>
