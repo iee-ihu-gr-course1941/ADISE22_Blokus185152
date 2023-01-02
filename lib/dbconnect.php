128cbe0fdb295df58202b2f7902db3fca1f114e2
Polihronis Varvaris <75493778+PolihronisVarvaris@users.noreply.github.com>
2023-01-02 20:48:25 +0200

    Update dbconnect.php

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -8,8 +8,9 @@ $pass=$DB_PASS;
 
 
 if(gethostname()=='users.iee.ihu.gr') {
-	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2018/it185152/mysql/run/mysql.sock');
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/staff/asidirop/mysql/run/mysql.sock');
 } else {
+		$pass=null;
         $mysqli = new mysqli($host, $user, $pass, $db);
 }
 
